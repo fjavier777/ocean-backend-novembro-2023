30c0d1a9c32e26ac7609756120e06aafc384939d
fjavier777 <113363561+fjavier777@users.noreply.github.com>
2023-11-09 13:16:01 +0000

    Atualização index

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
+
 const app = express()
 
-app.get('/', function (req, res) {
+ app.get('/', function (req, res) {
+
   res.send('Hello World')
+
 })
 
-app.get("/oi", function (req, res) {
-  res.send("Olá, mundo!")  
-});
+app.get("/oi", function ( req, res){
 
-app.listen(3000,
-console.log ("Servidor rodando em http:localhost/3000")
-);
+  res.send("olá mundo!")
+
+})
+
+app.listen(3000)
